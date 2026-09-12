6ab2feb1ad10c0b96798590f43902d4e803dd8f9
Volodymyr Shynkarov <3dmirok@gmail.com>
2026-09-12 02:17:32 -0500

    ci: deploy playground to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'pages' ? '/reveal-sequence/' : '/',
   build:
     mode === 'library'
       ? {
